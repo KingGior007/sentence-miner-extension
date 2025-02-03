c0808a19e73f3fac1f2a8d08bf0a6c2b647a7021
KingGior007 <gtzovairis@outlook.com>
2025-02-03 16:56:45 +0200

    Context menu only opens when a video is being watched

--- background.js
+++ background.js
@@ -2,8 +2,13 @@ chrome.runtime.onInstalled.addListener(function() {
 	chrome.contextMenus.create({
 		id: "manual-segmentation",
 		title: "Create word: \"%s\"",
-		contexts: ["selection"]
-	});
+		contexts: ["selection"],
+		documentUrlPatterns: [
+			"*://*.netflix.com/watch*",
+			"*://*.youtube.com/watch*"
+		]
+	}
+	);
 });
 chrome.contextMenus.onClicked.addListener((info, tab) => {
 	if (info.menuItemId === "manual-segmentation") {
