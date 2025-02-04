78696247c9176a22245ceade6366978ac9613f22
KingGior007 <gtzovairis@outlook.com>
2025-02-04 20:40:21 +0200

    Added the framework of adding known words

--- background.js
+++ background.js
@@ -1,3 +1,26 @@
+let open = indexedDB.open("known-words", 3);
+open.onupgradeneeded = (e) => {
+	let db = e.target.result;
+	db.deleteObjectStore("words");
+	let objectStore = db.createObjectStore("words", { keyPath: "id", autoIncrement: true });
+	objectStore.createIndex("word-index", "word", { unique: true });
+}
+
+open.onsuccess = (e) => {
+	let db = e.target.result;
+	let store = db.transaction("words", "readwrite").objectStore("words");
+	let request = store.add({ word: "word1", known: true });
+	request.onsuccess = function() {
+		console.log(request.result);
+
+		let req = store.index("word-index").get("word1");
+		req.onsuccess = function() {
+			console.log(req.result.word);
+			store.clear()
+		};
+	};
+}
+
 chrome.runtime.onInstalled.addListener(function() {
 	chrome.contextMenus.create({
 		id: "manual-segmentation",
